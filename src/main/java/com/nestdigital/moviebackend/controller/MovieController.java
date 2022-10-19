0a2bb2ba2a980411db2dce0e6ae6ef5a161d8174
Varshini <varshuk01@gmail.com>
2022-10-19 13:10:39 +0530

    api

--- src/main/java/com/nestdigital/moviebackend/controller/MovieController.java
+++ src/main/java/com/nestdigital/moviebackend/controller/MovieController.java
@@ -1,13 +1,23 @@
 package com.nestdigital.moviebackend.controller;
 
+import com.nestdigital.moviebackend.dao.MovieDao;
+import com.nestdigital.moviebackend.model.MovieModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
-    @PostMapping("/movieadd")
-    public String Movie(){
-        return "test";
+    @Autowired
+    private MovieDao dao;
+    @CrossOrigin(origins = "*")
+    @PostMapping(path="/movieadd",consumes = "application/json",produces = "application/json")
+    public String Movie(@RequestBody MovieModel movie){
+        System.out.println(movie.toString());
+        dao.save(movie);
+        return "{status:'success'}";
     }
 
 }
